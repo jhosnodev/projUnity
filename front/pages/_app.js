633b31bca823bb9359877299b3e56de9e3e2c57f
jhosno <jhosno.dev@gmail.com>
2023-09-26 20:09:24 -0400

    estableciendo redux

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,13 +1,18 @@
 import "../styles/globals.css";
 // import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
+import { wrapper, store } from "../redux/store";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <Provider store={store}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </Provider>
   );
 }
 
-export default MyApp;
+/* export default MyApp; */
+export default wrapper.withRedux(MyApp);
