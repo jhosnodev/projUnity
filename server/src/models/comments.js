d7c3810a10fec912380b26c377df5505884f4f7b
guspaz0 <guspaz0>
2023-10-01 16:49:14 -0300

    actualizo model comments

--- server/src/models/comments.js
+++ server/src/models/comments.js
@@ -1,34 +1,35 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize)=>{
-    sequelize.define("Comments",{        
-        id:{
-            type:DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull:false,
-            primaryKey: true,
-            unique: false
-        },
-        image:{
-            type:DataTypes.STRING,
-            allowNull:false,
-        },
-        active:{
-            type: DataTypes.BOOLEAN,
-            default: true,
-        },
-        replyTo:{
-            type: DataTypes.BOOLEAN,
-            default: true
-        },
-        belongToProyect:{
-            type: DataTypes.BOOLEAN,
-            default: true
-        }
-    },
-    {
-        timestamps: true,
-        freezeTablename: true
-    }
-    )
+        sequelize.define("Comments",{        
+            id:{
+                type:DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                allowNull:false,
+                primaryKey: true,
+                unique: false
+            },
+            image:{
+                type:DataTypes.STRING,
+                allowNull:false,
+            },
+            creationDate:{
+               type:DataTypes.RANGE(DataTypes.DATEONLY)
+            },
+            updateData:{
+                type:DataTypes.RANGE(DataTypes.DATEONLY)
+            },
+            active:{
+                type: DataTypes.BOOLEAN,
+                default: true,
+            },
+            replyTo:{
+                type: DataTypes.BOOLEAN,
+                default: true
+            },
+            belongToProyect:{
+                type: DataTypes.BOOLEAN,
+                default: true
+            }
+        })
 }
