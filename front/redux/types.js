6db3858b10c5c8e813ed538e5ada310d8632d482
Laura Grisanzio <lauragrisanzio2@gmail.com>
2023-09-30 21:04:18 -0300

    detail, component buttonDownload & comments

--- front/redux/types.js
+++ front/redux/types.js
@@ -2,10 +2,14 @@ const ADD_PROJECT = "ADD_PROJECT";
 const GET_ALL_PROJECTS = "GET_ALL_PROJECTS";
 const PROJECT_ERROR = "PROJECT_ERROR";
 const SET_ALERT = "SET_ALERT";
+//const GET_DETAIL = "GET_DETAIL"
+
 
 const actionsTypes = {
   ADD_PROJECT,
   GET_ALL_PROJECTS,
   PROJECT_ERROR,
+  // GET_DETAIL,
+  SET_ALERT
 };
 export default actionsTypes;
