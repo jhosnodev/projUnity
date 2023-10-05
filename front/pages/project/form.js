e4d9e35bb2306a334ebce5d35c76887487b50861
DaniloGomez5 <danilog992@gmail.com>
2023-10-05 16:36:36 -0300

    form projects listo/ form login listo(falta conectar)

--- front/pages/project/form.js
+++ front/pages/project/form.js
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { tags, categories, status } from "../api/data";
-/* import styles from "../../styles/form.module.css"; */
+import LayoutUser from "../../components/layoutUser";
+import Head from "next/head";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategory, addProjects } from "../../redux/actions/actions";
+import Loader from "../../components/loader";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {
   Textarea,
   Select,
@@ -11,34 +18,91 @@ import {
   Button,
   Input,
   CustomRadio,
-  radioGroup,
+  useSelect,
+  
 } from "@nextui-org/react";
-import LayoutUser from "../../components/layoutUser";
-import Head from "next/head";
+
+//========================================================================================
 
 const Form = () => {
   const [values, setValues] = useState({
     images: [],
   });
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm();
-
-  console.log(errors);
-
-  const handleOnChange = (event) => {
-    const { name, value } = event.target;
-    setValues({ ...values, [name]: value });
-  };
-
-  const onSubmit = handleSubmit((data) => {
-    console.log(data);
-    reset();
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      shortDescription: "",
+      price: "",
+      image: "",
+      description: "",
+      status: "",
+      category: "",
+      tags: "",
+      commentsAllowed: "",
+      visibility: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required("Tu proyecto necesita un titulo")
+        .min(3, "El titulo debe tener minimo 5 caracteres")
+        .max(20, "el nombre del proyecto no puede superar los 40 caracteres"),
+      shortDescription: Yup.string()
+        .required("Agrega una descripcion corta")
+        .min(15, "La descripcion debe contener al menos 15 caracteres")
+        .max(50, "La descripción no puede superar los 50 caracteres"),
+      price: Yup.number()
+      .required("Introduce un precio para tu proyecto")
+      .min(0, "El precio debe ser mayor o igual a 0"),
+      image: Yup.string().required("Tu proyecto necesita un cover"),
+      description: Yup.string()
+        .required("Cuenta a detalle tu proyecto")
+        .min(30, "El detalle debe contener al menos 30 caracteres")
+        .max(500, "El detalle no debe superar los 500 caracteres"),
+      status: Yup.string().required("Selecciona el estado de tu proyecto"),
+      category: Yup.string().required("Selecciona la categoria de tu proyecto"),
+      tags: Yup.string().required("Selecciona tags/etiquetas para tu proyecto"),
+      commentsAllowed: Yup.string().required("Selecciona una opcion"),
+      visibility: Yup.string().required("Selecciona una opcion"),
+    }),
+    onSubmit: (values) => {
+      try {
+        const post = {
+          ...values,
+          tags: values.tags.split(",").map( tag => parseInt(tag)),
+          price: parseFloat(values.price),
+          category: parseInt(values.category),
+          view: 0,
+          commentsAllowed : values.commentsAllowed === 'true' ? true : false,
+          visibility : values.visibility === 'true' ? true : false
+        };
+        dispatch(addProjects(values));
+        console.log(values);
+        formik.resetForm();
+        toast.success('Tu Proyecto se publicó con éxito!');
+      } catch (error) {
+        console.error(error);
+        toast.error('Hubo un error al procesar la solicitud');
+      }
+    },
   });
+
+  useEffect(() => {
+    formik.validateForm();
+  }, [formik.values]);
+
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(getCategory());
+  }, [dispatch]);
+  /*   const categories = useSelector((state) => state.projectsData.categories); */
+  const loading = useSelector((state) => state.projectsData.loading);
+  const alert = useSelector((state) => state.projectsData.alert);
+
+  console.log(alert);
+  if (loading) return <Loader />;
+
+
 
   return (
     <LayoutUser>
@@ -48,7 +112,7 @@ const Form = () => {
       </Head>
       <div className="flex justify-center">
         <form
-          onSubmit={onSubmit}
+          onSubmit={formik.handleSubmit}
           className="p-6 flex flex-col gap-11 bg-background-100  w-12/12 md:w-8/12"
           encType="multipart/form-data"
         >
@@ -61,70 +125,35 @@ const Form = () => {
               defaultValue=""
               variant="faded"
               name="name"
-              {...register("name", {
-                required: {
-                  value: true,
-                  message: "Tu proyecto necesita un titulo",
-                },
-                minLength: {
-                  value: 3,
-                  message: "El titulo debe tener minimo 3 caracteres",
-                },
-                maxLength: {
-                  value: 20,
-                  message:
-                    "el nombre del proyecto no puede superar los 20 caracteres",
-                },
-              })}
+              {...formik.getFieldProps("name")}
             />
-            {errors.name && <span>{errors.name.message}</span>}
-          </div>
-
-          {/* <div className={styles.input}>
-            <Input
-              isRequired
-              type="text"
-              label="Enter URL of project"
-              defaultValue=""
-              name="url"
-            />
-          </div> */}
-
-          <div>
-            <Input
-              type="text"
-              label="Descripcion corta de tu proyecto:"
-              defaultValue=""
-              variant="faded"
-              name="shortDescription"
-              {...register("shortDescription", {
-                required: {
-                  value: true,
-                  message: "Agrega una descripcion corta",
-                },
-                minLength: {
-                  value: 5,
-                  message: "La descripcion debe contener al menos 5 caracteres",
-                },
-                maxLength: {
-                  value: 30,
-                  message: "La descripción no puede superar los 30 caracteres",
-                },
-              })}
-            />
-            {errors.shortDescription && (
-              <span>{errors.shortDescription.message}</span>
+            {formik.touched.name && formik.errors.name && (
+              <span>{formik.errors.name}</span>
             )}
           </div>
 
           <div>
             <Input
-              label="Precio"
+              type="text"
+              label="Short Description of project"
+              defaultValue=""
+              variant="faded"
+              name="shortDescription"
+              {...formik.getFieldProps("shortDescription")}
+            />
+            {formik.touched.shortDescription && formik.errors.shortDescription && (
+              <span>{formik.errors.shortDescription}</span>
+            )}
+          </div>
+
+          <div>
+            <Input
+              label="Price"
               placeholder="0.00"
               labelPlacement="outside"
               name="price"
               variant="faded"
-              {...register("price", { required: true })}
+              {...formik.getFieldProps("price")}
               startContent={
                 <div className="pointer-events-none flex items-center">
                   <span className="text-default-400 text-small">$</span>
@@ -146,27 +175,11 @@ const Form = () => {
               }
               type="number"
             />
-            {errors.price && <span>Introduce un precio para tu proyecto</span>}
+            {formik.touched.price && formik.errors.price && (
+              <span>{formik.errors.price}</span>
+            )}
           </div>
 
-          <div>
-            {/* <input
-              type="file"
-              name="cover"
-              accept="image/*"
-              onChange={handleOnChange}
-            /> */}
-
-            <input
-              type="file"
-              name="images"
-              multiple
-              accept="image/*"
-              onChange={handleOnChange}
-              {...register("images", { required: true })}
-            />
-            {errors.images && <span>Tu proyecto necesita una imagen</span>}
-          </div>
           <div>
             <Input
               type="text"
@@ -175,43 +188,35 @@ const Form = () => {
               variant="faded"
               placeholder="URL de cover del proyecto (imagen PNG/JPG)"
               name="image"
-              {...register("image", { required: true })}
+              {...formik.getFieldProps("image")}
             />
-            {errors.image && <span>Tu proyecto necesita un cover</span>}
+            {formik.touched.image && formik.errors.image && (
+              <span>{formik.errors.image}</span>
+            )}
           </div>
 
           <div>
             <Textarea
+              label="Enter long description"
               labelPlacement="outside"
-              placeholder="Introduce una descripcion mas detallada de tu proyecto:"
+              placeholder="Enter description of the project"
               name="description"
               variant="faded"
-              {...register("description", {
-                required: {
-                  value: true,
-                  message: "Cuenta a detalle tu proyecto",
-                },
-                minLength: {
-                  value: 30,
-                  message: "El detalle debe contener al menos 30 caracteres",
-                },
-                maxLength: {
-                  value: 300,
-                  message: "El detalle no debe superar los 300 caracteres",
-                },
-              })}
+              {...formik.getFieldProps("description")}
             />
-            {errors.description && <span>{errors.description.message}</span>}
+            {formik.touched.description && formik.errors.description && (
+              <span>{formik.errors.description}</span>
+            )}
           </div>
 
           <div>
             <Select
-              label="Status del proyecto"
-              placeholder="Selecciona una opcion"
+              label="Project status"
+              placeholder="Select an status"
               defaultSelectedKeys=""
               name="status"
               variant="faded"
-              {...register("status", { required: true })}
+              {...formik.getFieldProps("status")}
             >
               {status.map((status) => (
                 <SelectItem key={status.value} value={status.value}>
@@ -219,26 +224,28 @@ const Form = () => {
                 </SelectItem>
               ))}
             </Select>
-            {errors.status && <span>Selecciona el estado de tu proyecto</span>}
+            {formik.touched.status && formik.errors.status && (
+              <span>{formik.errors.status}</span>
+            )}
           </div>
 
           <div>
             <Select
-              label="Categoria del proyecto"
-              placeholder="Selecciona una opcion"
+              label="Project Category"
+              placeholder="Select an category"
               defaultSelectedKeys=""
               name="category"
               variant="faded"
-              {...register("category", { required: true })}
+              {...formik.getFieldProps("category")}
             >
-              {categories.map((categories) => (
-                <SelectItem key={categories.value} value={categories.value}>
-                  {categories.label}
+              {categories.map((item) => (
+                <SelectItem key={item.value} value={item.value}>
+                  {item.label}
                 </SelectItem>
               ))}
             </Select>
-            {errors.category && (
-              <span>Selecciona la categoria de tu proyecto</span>
+            {formik.touched.category && formik.errors.category && (
+              <span>{formik.errors.category}</span>
             )}
           </div>
 
@@ -247,60 +254,67 @@ const Form = () => {
               label="Tags"
               placeholder="Selecciona tus tags"
               defaultSelectedKeys=""
-              name="category"
+              name="tags"
               variant="faded"
               isMultiline
               selectionMode="multiple"
-              {...register("tags", { required: true })}
+              {...formik.getFieldProps("tags")}
             >
               {tags.map((tag) => (
-                <SelectItem key={tag.lavel} value={tag.lavel}>
-                  {tag.lavel}
+                <SelectItem key={tag.value} value={tag.value}>
+                  {tag.label}
                 </SelectItem>
               ))}
             </Select>
-            {errors.tags && (
-              <span>Selecciona tags/etiquetas para tu proyecto</span>
+            {formik.touched.tags && formik.errors.tags && (
+              <span>{formik.errors.tags}</span>
             )}
           </div>
 
           <div>
             <RadioGroup
-              label="Permitir comentarios"
+              label="Allow comments"
               orientation="horizontal"
               name="commentsAllowed"
+              {...formik.getFieldProps("commentsAllowed")}
             >
-              <Radio value="Yes" className="Yes" {...register("commentsAllowed", { required: true })}>
-                Si
+              <Radio value="true" className="commentsAllowed">
+                Yes
               </Radio>
-              <Radio value="No" className="No" {...register("commentsAllowed", { required: true })}>
+              <Radio value="false" className="commentsAllowed">
                 No
               </Radio>
             </RadioGroup>
-            {errors.commentsAllowed && <span>Selecciona una opcion</span>}
+            {formik.touched.commentsAllowed && formik.errors.commentsAllowed && (
+              <span>{formik.errors.commentsAllowed}</span>
+            )}
           </div>
 
           <div>
             <RadioGroup
-              label="Visibilidad"
+              label="Visibility"
               orientation="horizontal"
               name="visibility"
+              {...formik.getFieldProps("visibility")}
             >
-              <Radio value="Public" className="public" {...register("visibility", { required: true })}>
-                Publico
+              <Radio value="false" className="visibility" >
+                Only me
               </Radio>
-              <Radio value="Only me" className="onlyMe" {...register("visibility", { required: true })}>
-                Solo para mi
+              <Radio value="true" className="visibility" >
+                Public
               </Radio>
             </RadioGroup>
-            {
-              errors.visibility && <span>Selecciona una opcion</span>
-            }
+            {formik.touched.visibility && formik.errors.visibility && (
+              <span>{formik.errors.visibility}</span>
+            )}
           </div>
 
-          <input type="submit" value="Enviar" />
+          <input
+            type="submit"
+            className="w-2/12  justify-self-end self-end p-2 bg-primary rounded-md text-background cursor-pointer"
+          />
           {/* <Button
-            color="primary"
+            color="primry"
             
             className="w-2/12  justify-self-end self-end"
           >
