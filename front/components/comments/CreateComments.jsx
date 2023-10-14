bd39516b905b9ec9a92908294bee31cc95c635f1
jhosno <jhosno.dev@gmail.com>
2023-10-14 02:00:25 -0400

    Update CreateComments.jsx

--- front/components/comments/CreateComments.jsx
+++ front/components/comments/CreateComments.jsx
@@ -1,39 +1,35 @@
 import { Button } from "@nextui-org/react";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
-const CreateComments = ({comments}) => {
-
-   const [commentsData, setComments] = useState({
-     user: "",
-     comments: "",
-   });
-  console.log(comments);
+const CreateComments = ({ project }) => {
+  const [commentsData, setComments] = useState({
+    user: "",
+    comments: "",
+    project: project,
+    image:
+      "https://blog.openreplay.com/images/building-a-comment-form-with-react-mentions/images/hero.png",
+    active: true,
+    replyTo: true,
+  });
   //  const [errors, setErrors] = useState({});
 
-   const handleChange = (event) => {
-     const property = event.target.name;
-     const value = event.target.value;
-    //  setErrors(validator({ ...userData, [property]: value }));
-    //  //seteamos los errores en nuestro estado
-     setComments({ ...commentsData, [property]: value });
-   };
+  const handleChange = (event) => {
+    const property = event.target.name;
+    const value = event.target.value;
 
-  
-  //  const handleSubmit = (event) => {
-  //    event.preventDefault();
-  //    print();
-  //  };
+    setComments({ ...commentsData, [property]: value });
+  };
+
+  const handleSubmitComment = (event) => {
+    e.preventDefault();
+    console.log("envando ");
+  };
+
+  console.log(commentsData);
   return (
     <div>
       <form className="flex flex-col ml-3">
-        {/* <input
-          class="m-1 w-52 shadow appearance-none py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          name="user"
-          placeholder="Username..."
-          onChange={(e) => handleChange(e)}
-          value={commentsData.user}
-        /> */}
         <input
           className="mr-3 h-14 w-full shadow appearance-none py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
@@ -44,10 +40,9 @@ const CreateComments = ({comments}) => {
         />
         <div className="flex flex-row justify-end m-3">
           <Button
-
             margin="1"
             color="primary"
-            onPress={() => alert("comment projects")}
+            onPress={(e) => handleSubmitComment(e)}
           >
             Commentar
           </Button>
