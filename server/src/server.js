93f0b7d5347e0c0cbfcfbdd02329f660e9722cae
DracoAlex84 <draco_alexis@hotmail.com>
2023-10-16 21:02:20 -0300

    Prueba3

--- server/src/server.js
+++ server/src/server.js
@@ -6,33 +6,32 @@ const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const server = express();
 
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(cors());
-
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Credentials', 'true'); 
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   res.header(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, OPTIONS, PUT, DELETE'
-//   )
-//   next();
-// });
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true'); 
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, DELETE'
+  )
+  next();
+});
 
 
 server.set('views', __dirname+'/views')
 server.set('view engine', 'ejs')
 server.use(morgan("dev"));
+server.use(cors());
 server.use(require('express-session')({
   secret: process.env.SESSION_KEY,
   resave: false,
