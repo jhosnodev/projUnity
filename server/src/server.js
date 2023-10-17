128b71372ab402478ab82bde0cc09e7e74a8b9b7
DracoAlex84 <draco_alexis@hotmail.com>
2023-10-16 21:00:53 -0300

    Prueba2

--- server/src/server.js
+++ server/src/server.js
@@ -13,25 +13,26 @@ const server = express();
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true'); 
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, DELETE'
-  )
-  next();
-});
+server.use(cors());
+
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Credentials', 'true'); 
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   res.header(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, OPTIONS, PUT, DELETE'
+//   )
+//   next();
+// });
 
 
 server.set('views', __dirname+'/views')
 server.set('view engine', 'ejs')
 server.use(morgan("dev"));
-server.use(cors());
 server.use(require('express-session')({
   secret: process.env.SESSION_KEY,
   resave: false,
