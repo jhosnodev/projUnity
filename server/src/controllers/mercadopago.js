d0bf80a7155ca9914a74564e6e0e29e9ad5d4bf8
guspaz0 <guspaz0>
2023-10-23 20:01:01 -0300

    modificaciones al payments y dashboard

--- server/src/controllers/mercadopago.js
+++ server/src/controllers/mercadopago.js
@@ -3,15 +3,13 @@ const {MP_TOKEN, DB_HOST} = process.env
 const { Order_detail , Order, Payments, Users, Projects } = require('../db.js');
 const paymentsServices = require('../services/payment.js');
 const {Sequelize} = require('sequelize');
-const { projects } = require('../utils/index.js');
-
 
 // Configura las credenciales de MercadoPago
 const paymenntsControllers = {
 
     // Función para crear una preferencia de pago en MercadoPago
   createPaymentPreference:  async function(req, res )  {
-    // const { items, payer, concepto, status } = req.body; 
+    const { items, payer, concepto, status } = req.body; 
     mercadopago.configure({
       access_token: MP_TOKEN
     });
@@ -20,58 +18,6 @@ const paymenntsControllers = {
       attributes: [Sequelize.fn('max', Sequelize.col('orderNumber'))],
       raw: true
     })
-    compra = req.body
-    items = [
-      {  
-        buyer:compra[0].UserId,
-        id:compra[0].projectId,
-         title: compra[0].title,
-         currency_id: 'ARS',
-         unit_price:Number(compra[0].unit_price),
-         quantity: 1,
-        
-    },
-   {  
-        buyer:compra[1].UserId,
-        id:compra[1].projectId,
-        title: compra[1].title,
-        currency_id: 'ARS',
-        unit_price:Number(compra[1].unit_price),
-        quantity: 1,
-       
-},
-{  
-        buyer:compra[2].UserId,
-        id:compra[2].projectId,  
-        title: compra[2].title,
-        currency_id: 'ARS',
-        unit_price:Number(compra[2].unit_price),
-        quantity: 1,
-        
-},
-    ]
-    const preference = {
-      items,
-      total_amount: 1,
-      external_reference : `${orderNumber[0].max+1}`,
-      back_urls: {
-        success: "http://localhost:3001/createPayment/succes",
-        pending: `${DB_HOST}/error`,
-        failure: `${DB_HOST}/pending`,
-      },
-      notification_url: "https://3eb3-181-29-72-133.ngrok.io/webhook",
-      auto_return: "approved" 
-    };
-    // console.log(preference)
-      try {
-        const response = await mercadopago.preferences.create(preference);
-        // console.log(response.body);
-        global.id = response.body.id;
-        init_point = response.body.init_point;
-       const totalPrecio = items.reduce((acumulador, producto) =>
-      acumulador + parseFloat(producto.unit_price), 0);
-        
-     console.log(items)
 
     const orderNumber = lastOrderNumber[0].max+1
 
@@ -86,16 +32,8 @@ const paymenntsControllers = {
       })
     }
 
-    // const items = [
-    //   {  
-    //     id: projectId,
-    //     title: title,
-    //     unit_price:Number(unit_price),
-    //     quantity: 1
-    //   },
-    // ] 
-    // const totalPrecio = items.reduce((acumulador, producto) =>
-    //   acumulador + parseFloat(producto.unit_price), 0);
+    const totalPrecio = items.reduce((acumulador, producto) =>
+      acumulador + parseFloat(producto.unit_price), 0);
 
     const preference = {
       items,
@@ -106,11 +44,7 @@ const paymenntsControllers = {
         attributes: ['name', 'email'],
         raw: true
       }),
-      // payer: {
-      //   name: "Lalo",
-      //   surname: "Landa",
-      //   email: "test_user_63274575@testuser.com",
-      // },
+
       back_urls: {
         success: "http://localhost:3001/createPayment/succes",
         pending: `${DB_HOST}/error`,
@@ -121,7 +55,7 @@ const paymenntsControllers = {
     };
       try {
         const response = await mercadopago.preferences.create(preference);
-        // console.log(response.body);
+        //console.log(response.body);
 
         global.id = response.body.id;
         init_point = response.body.init_point;
@@ -146,11 +80,8 @@ const paymenntsControllers = {
             }
           });
         }
-
         const queryOrder = await Payments.findAll({where: {orderNumber: orderNumber}, raw: true})
-
         let itemsDb = []
-
         for (let i in queryOrder) {
           let { product, paymentAmount} = queryOrder[i]
           let productName = await Projects.findOne({where: {id: product}, attributes: ['name'], raw: true})
@@ -170,8 +101,7 @@ const paymenntsControllers = {
       } catch (error) {
         console.log(error);
       }
-    }
-  },
+    },
         // res.json({
         //   id_mercadopago: global.id,
         //   init_point: response.body.init_point,
@@ -191,8 +121,8 @@ const paymenntsControllers = {
     },
     getAllPayment: async function(req, res){
       try {
-        const paymentsData = req.body;
-        const allPayments = await paymentsServices.allPayments(paymentsData);
+        const paymentsData = req.body; // <<< para que esta??
+        const allPayments = await paymentsServices.allPayments();
           res.status(200).json(allPayments)
       } catch (error) {
           res.status(500).json(error.message)
@@ -201,4 +131,4 @@ const paymenntsControllers = {
     }
   };
         
-module.exports =  paymenntsControllers 
+module.exports =  paymenntsControllers
