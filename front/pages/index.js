4d4b204bc8f9c0e80d26bee470104fb66b5c82a1
DaniloGomez5 <danilog992@gmail.com>
2023-10-24 20:47:46 -0300

    Login con github 100% real funcionando
    y otros cambios...

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { use } from "react";
 import ProjectCarousel from "../components/layout/carousel";
 import { useDispatch, useSelector } from "react-redux";
 import ProjectCard from "../components/project/ProjectCard";
@@ -10,9 +10,26 @@ import solicitudes from "../components/solicitudesCom.json";
 import { Link } from "@nextui-org/react";
 import Loader from "../components/layout/loader";
 import Head from "next/head";
+import { parseCookies } from "nookies";
+import axios from "axios";
 
-export default function Home({}) {
+export default function Home(props) {
+  console.log("props linea 16", props);
   const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (props.user && typeof props.user.id === "number") {
+      console.log("props.user.id", props.user.id);
+      localStorage.setItem("sesion", JSON.stringify(props.user));
+      dispatch({
+        type: "LOGIN",
+        payload: {
+          data: props.user,
+          alert: { type: "success", msg: "Inicio de sesion exitoso!" },
+        },
+      });
+    }
+  }, [props.user]);
 
   const projects = useSelector((state) => state.projectsData.projectsFilter);
   React.useEffect(() => {
@@ -42,6 +59,12 @@ export default function Home({}) {
             ))}
           </div>
 
+          {!props?.authorization && (
+            <a href={"http://localhost:3001/auth/github"}>
+              Click here to login
+            </a>
+          )}
+
           <div className="gap-9 grid grid-cols-1 md:grid-cols-4 sm:grid-cols-2 p-4 justify-center content-center items-center mt-4">
             <h3>Aun no encuentras lo que buscas?</h3>
             <Link href={"/browser"}>
@@ -58,7 +81,7 @@ export default function Home({}) {
         </div>
         <div className="my-9 mx-4">
           <Link href="/community" className="mb-4">
-          <h1 className="pb-2">Solicitudes de la comunidad</h1>
+            <h1 className="pb-2">Solicitudes de la comunidad</h1>
           </Link>
           <div className="gap-6 flex flex-row  w-full">
             {solicitudes.data.map((solicitud) => (
@@ -70,3 +93,33 @@ export default function Home({}) {
     </LayoutUser>
   );
 }
+
+async function getUser(authorization) {
+  let res = null;
+  await fetch("http://localhost:3001/profile", {
+    headers: { authorization },
+  })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log("responseJSON:", responseJson);
+      if (responseJson.id) {
+        /* const sesionUsuario = { ...props.user, access: true }; */
+        res = { authorization, user: { ...responseJson, access: true }};
+      } else {
+        res = { authorization };
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  console.log("res es", res);
+  return res;
+}
+
+Home.getInitialProps = async (ctx) => {
+  const { authorization } = parseCookies(ctx);
+  const { token } = ctx.query;
+
+  const props = await getUser(authorization || token);
+  return props;
+};
