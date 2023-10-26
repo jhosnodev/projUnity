d480c5825defc9353c8dffb0c157388094271023
guspaz0 <gusti.paz@gmail.com>
2023-10-26 13:27:04 -0300

    ok

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -3,12 +3,23 @@ const Service = require('../services').userServices;
 const userControllers = {
     getUsers: async function (req,res) {
         try {
-            const { name } = req.query
-            const Users = await Service.allUsers(name)
+            const { name, deleted } = req.query
+            const Users = await Service.allUsers({name, deleted})
             res.status(200).json(Users)
         } catch (error) {
             res.status(500).json(error.message)
         }
+    },
+    getUserById: async function(req,res) {
+      try {
+        const {id} = req.params;
+        const User = await Service.userById(id)
+        if (User) {
+          res.status(200).json(User)
+        }
+      } catch (error) {
+        res.status(500).json(error.message)
+      }
     },
     postUser: async function (req,res) {
         try {
@@ -28,7 +39,7 @@ const userControllers = {
         try {
           const userId = req.params.id;
     
-          const result = await Service.deleteUser(userId);
+          const result = await Service.deleteUserbyId(userId);
     
           res.status(200).json(result);
         } catch (error) {
@@ -38,8 +49,8 @@ const userControllers = {
   
       restoreUser : async function (req, res) {
         try{
-          const userId = req.params.id;
-          const result = await Service.restoreUsers(userId);
+          const {id} = req.params;
+          const result = await Service.restoreUsers(id);
           res.status(200).json(result);
         }catch(error){
           res.status(500).json(error.message);
--- server/src/services/Users.js
+++ server/src/services/Users.js
@@ -1,10 +1,14 @@
-const { Users } = require("../db");
-const { Op } = require("sequelize");
-var pbkdf2 = require("pbkdf2");
+const { Users } = require('../db');
+const {Op} = require('sequelize');
+var pbkdf2 = require('pbkdf2');
 var salt = process.env.SALT_KEY;
 const cloudinary = require("cloudinary").v2;
 
-const { CB_CLOUD_NAME, CB_API_KEY, CB_API_SECRET } = process.env;
+const {
+  CB_CLOUD_NAME,
+  CB_API_KEY ,
+  CB_API_SECRET,
+} = process.env;
 
 cloudinary.config({
   cloud_name: CB_CLOUD_NAME,
@@ -13,198 +17,159 @@ cloudinary.config({
   secure: true,
 });
 
+
 function encryptionPassword(password) {
-  var key = pbkdf2.pbkdf2Sync(password, salt, 36000, 64, "sha256");
-  var hash = key.toString("hex");
-  return hash;
+    var key = pbkdf2.pbkdf2Sync(
+        password, salt, 36000, 64, 'sha256'
+    );
+    var hash = key.toString('hex');
+    return hash;
 }
 
+
 const userServices = {
-  allUsers: async function (name) {
-    try {
-      if (name) {
-        const response = await Users.findAll({
-          where: {
-            name: { [Op.iLike]: `%${name}%` },
-            [Op.or]: [{ name: { [Op.iLike]: `${name}%` } }],
-            [Op.and]: [{ active: "true" }],
-          },
-          attributes: [
-            "id",
-            "name",
-            "email",
-            "image",
-            "twitterUser",
-            "emailUser",
-            "githubUser",
-            "linkedinUser",
-            "role",
-          ],
-        });
-        return response;
-      } else {
-        const response = await Users.findAll({
-          where: { active: "true" },
-          attributes: [
-            "id",
-            "name",
-            "email",
-            "image",
-            "twitterUser",
-            "emailUser",
-            "githubUser",
-            "linkedinUser",
-            "role",
-          ],
-        });
-        return response;
-      }
-    } catch (error) {
-      return error;
-    }
-  },
-  createUser: async function (userData) {
-    try {
-      const {
-        name,
-        email,
-        password,
-        image,
-        twitterUser,
-        emailUser,
-        githubUser,
-        linkedinUser,
-        role,
-      } = userData;
+    allUsers: async function (queryParams) {
+        try{
+          const { name, deleted } = queryParams
 
-      if (
-        !name ||
-        !email ||
-        !password /* || !image || !twitterUser || !emailUser || !githubUser <<== MODIFIQUE ESTO PARA PODER CREAR USUARIOS */ ||
-        !role
-      ) {
-        throw Error(`Missing some data`);
-      } else {
-        
-          //const uploadedImage = await cloudinary.uploader.upload(image);
-        
-
-        const [newUser, created] = await Users.findOrCreate({
-          where: { email: email },
-          defaults: {
-            name,
-            password: encryptionPassword(password),
-            //image: uploadedImage.secure_url,
-            image,
-            twitterUser,
-            emailUser,
-            githubUser,
-            role,
-            linkedinUser,
-          },
-        });
-        if (created) {
-          let {
-            id,
-            name,
-            email,
-            image,
-            twitterUser,
-            emailUser,
-            githubUser,
-            linkedinUser,
-            role,
-          } = newUser;
-          return {
-            id,
-            name,
-            email,
-            image,
-            twitterUser,
-            emailUser,
-            githubUser,
-            linkedinUser,
-            role,
-          };
-        } else {
-          throw Error("El email de usuario ya existe");
+          const response = await Users.findAll({
+              where: name? {
+                name: { [Op.iLike]: `%${name}%` },
+                [Op.or]: [{ name: { [Op.iLike]: `${name}%` } }],
+              } : null,
+              paranoid: deleted? false : true,
+              attributes: {exclude: ['password']},
+          })
+          return response
+        } catch (error) {
+            return error
         }
+    },
+    userById: async function(id) {
+      try {
+        const userId = await Users.findOne({
+          where: {id: id},
+          attributes: {exclude: ['password']}
+        });
+        if (userId) {
+          return userId
+        } else {
+          return 'User not Found'
+        }
+      } catch (error) {
+        return error
       }
-    } catch (error) {
-      return error;
-    }
-  },
+    },
+    createUser: async function (userData) {
+        try {
+            const { name, email, password, image, twitterUser, emailUser, githubUser, linkedinUser, role} = userData
 
-  updateUser: async function (userData, res) {
-    try {
-      const {
-        id,
-        name,
-        email,
-        password,
-        image,
-        twitterUser,
-        emailUser,
-        githubUser,
-        linkedinUser,
-        roleId,
-      } = userData;
-      // find the user by ID
-      const user = await Users.findByPk(id);
-      if (!user) {
-        throw new Error("User not found");
-      }
-      // update the user data
-      user.name = name || user.name;
-      user.email = email || user.email;
-      user.password = password ? encryptionPassword(password) : user.password;
-      user.twitterUser = twitterUser || user.twitterUser;
-      user.emailUser = emailUser || user.emailUser;
-      user.githubUser = githubUser || user.githubUser;
-      user.linkedinUser = linkedinUser || user.linkedinUser;
-      user.roleId = roleId || user.roleId;
+            if ( !name || !email || !password /* || !image || !twitterUser || !emailUser || !githubUser <<== MODIFIQUE ESTO PARA PODER CREAR USUARIOS */ || !role) {
 
-      // upload the image to Cloudinary
-      if (image) {
-        const uploadedImage = await cloudinary.uploader.upload(image);
-        user.image = uploadedImage.secure_url;
-      }
+                throw Error(`Missing some data`)
+            } else {
 
-      // save the changes
-      await user.save();
+                const uploadedImage = await cloudinary.uploader.upload(image);
 
-      // return the updated user object
-      res.status(200).json(user);
-    } catch (error) {
-      console.log(error);
-      return error;
-    }
-  },
-  deleteUser: async function (userId) {
-    try {
-      const user = await Users.findByPk(userId);
-      if (!user) {
-        throw new Error("User not found");
-      }
-      await user.destroy();
-      return { message: "User deleted successfully" };
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  },
+                const [newUser, created] = await Users.findOrCreate({
+                    where: {email: email},
+                    defaults: {
+                        name,
+                        email,
+                        password: encryptionPassword(password),
+                        image: uploadedImage.secure_url,
+                        twitterUser,
+                        emailUser,
+                        githubUser,
+                        role,
+                        linkedinUser
+                    }
+                })
+                if (created) {
+                    let { id, name, email, image, twitterUser,emailUser, githubUser, linkedinUser, role } = newUser
+                    return { id, name, email, image, twitterUser, emailUser, githubUser, linkedinUser, role }
+                } else {
+                    throw Error('El email de usuario ya existe')
+                }
+            }
+        } catch (error){
+            return error
+        }
+    },
 
-  restoreUsers: async function (userId) {
-    try {
-      const user = await Users.findByPk(userId, { paranoid: false });
-      if (!user) {
-        throw new Error("User not found");
-      }
-      await user.restore();
-      return { message: "User restored successfully" };
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  },
-};
+    updateUser: async function (userData, res){
+        try {
+            const { id, name, email, password, image, twitterUser, emailUser, githubUser, linkedinUser, roleId} = userData
+            // find the user by ID
+            const user = await Users.findByPk(id);
+            if (!user) {
+                throw new Error("User not found");
+            }
+            // update the user data
+            user.name = name || user.name;
+            user.email = email || user.email;
+            user.password = password ? encryptionPassword(password) : user.password;
+            user.twitterUser = twitterUser || user.twitterUser;
+            user.emailUser = emailUser || user.emailUser;
+            user.githubUser = githubUser || user.githubUser;
+            user.linkedinUser = linkedinUser || user.linkedinUser;
+            user.roleId = roleId || user.roleId;
+   
+            // upload the image to Cloudinary
+            if (image) {
+              const uploadedImage = await cloudinary.uploader.upload(image);
+              user.image = uploadedImage.secure_url;
+            }
+   
+            // save the changes
+            await user.save();
+   
+            // return the updated user object
+            res.status(200).json(user);
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    deleteUserbyId: async function(userId) {
+        try {
+          const user = await Users.findByPk(userId);
+          if (!user) {
+            throw new Error('User not found');
+          }
+          await user.destroy();
+          return { message: 'User deleted successfully' };
+        } catch (error) {
+          throw new Error(error.message);
+        }
+      },
+    
+      getDeletedUsers: async function () {
+        try {
+            const deletedUsers = await Users.findAll({
+                paranoid: false,
+                attributes: {exclude: ['password']}
+            });
+            
+            return deletedUsers;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    },
 
-module.exports = userServices;
+      restoreUsers: async function(userId) {
+        try {
+          const user = await Users.findOne({where: {id: userId}, paranoid: false});
+          if (!user) {
+            throw new Error('User not found');
+          }
+          await user.restore();
+          return { message: 'User restored successfully' };
+        } catch (error) {
+          throw new Error(error.message);
+        }
+      },
+    
+}
+
+module.exports = userServices
