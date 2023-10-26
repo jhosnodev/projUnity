0f5d1022f4ccff1ee70f4853fd96aceb80599fd1
Gustavo Rodolfo Paz <gusti.paz@gmail.com>
2023-10-26 12:36:18 -0300

    Update users.js
    update controllers usuarios

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,27 +1,35 @@
 const Service = require('../services').userServices;
-const { sendEmail } = require('./mailer');
 
 const userControllers = {
     getUsers: async function (req,res) {
         try {
-            const { name } = req.query
-            const Users = await Service.allUsers(name)
+            const { name, deleted } = req.query
+            const Users = await Service.allUsers({name, deleted})
             res.status(200).json(Users)
         } catch (error) {
             res.status(500).json(error.message)
         }
     },
+    getUserById: async function(req,res) {
+      try {
+        const {id} = req.params;
+        const User = await Service.userById(id)
+        if (User) {
+          res.status(200).json(User)
+        }
+      } catch (error) {
+        res.status(500).json(error.message)
+      }
+    },
     postUser: async function (req,res) {
         try {
             const Users = await Service.createUser(req.body)
-            const userMail = Users.email
-            const subject = "Usuario creado con éxito ✔";
-            const text = `Hola ${Users.name}, te damos la bienvenida a PROJUNITY!.`;
-            const html = "<b>Bievenido a la comunidad de PROJUNITY!</b>"
-            sendEmail(userMail, subject, text, html)
-
-            res.status(200).json(Users)
-            console.log(Users);
+            if (Users.id) {
+              res.status(201).json(Users);
+            } else {
+              res.status(400).json({ type: "error", response: "Algo falló" });
+            }
+            console.log(Users)
         } catch (error) {
             res.status(500).json(error.message)
         }
@@ -32,27 +40,17 @@ const userControllers = {
           const userId = req.params.id;
     
           const result = await Service.deleteUserbyId(userId);
-  
+    
           res.status(200).json(result);
         } catch (error) {
           res.status(500).json(error.message);
         }
       },
-
-       getDeletedUsers: async function (req, res) {
-        try {
-            const deletedUsers = await Service.getDeletedUsers();
-            res.status(200).json(deletedUsers);
-        } catch (error) {
-            res.status(500).json(error.message);
-        }
-    },
-    
   
       restoreUser : async function (req, res) {
         try{
-          const userId = req.params.id;
-          const result = await Service.restoreUsers(userId);
+          const {id} = req.params;
+          const result = await Service.restoreUsers(id);
           res.status(200).json(result);
         }catch(error){
           res.status(500).json(error.message);
