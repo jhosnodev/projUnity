c17ab78b80c1cdd85bb094f57d489025b480d6af
Gustavo Rodolfo Paz <gusti.paz@gmail.com>
2023-10-27 13:28:25 -0300

    Update projects.js

--- server/src/services/projects.js
+++ server/src/services/projects.js
@@ -127,18 +127,14 @@ const ProjectServices = {
         ],
         where: condition.project,
         paranoid: false,
-        
       });
       let changeDeletedAt = []
       for (let i in projectsFilter) {
         changeDeletedAt = [
           ...changeDeletedAt,
           {
-            ...projectsFilter[i],
-            deletedAt: projectsFilter[i].deletedAt !== null? true : false
-            Categories: [
-              {  }
-            ]
+            ...projectsFilter[i].get({plain:true}),
+            deletedAt: projectsFilter[i].deletedAt !== null? true : false,
           }
         ]
       }
